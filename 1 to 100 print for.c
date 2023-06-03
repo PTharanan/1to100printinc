84ce400f31587dbac0f1b0eb50e1885fa0cdefbe
Tharanan <108599220+PTharanan@users.noreply.github.com>
2023-06-03 17:48:15 +0530

    Add files via upload

---
+++ 1 to 100 print for.c
@@ -0,0 +1,20 @@
+/*
+ ============================================================================
+ Name        : 1.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	int a;
+	for(a=1;a<=100;a++){
+		printf("%d\n",a);
+	}
+	return EXIT_SUCCESS;
+}
